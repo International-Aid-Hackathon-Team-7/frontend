0e3ce7a917e3aa24635dc7613933605a858c7ea9
jmolasky <jeromemolasky@gmail.com>
2022-05-04 15:03:09 -0500

    adds basic forum carousel

--- src/pages/Forum/Forum.jsx
+++ src/pages/Forum/Forum.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import styles from './Forum.module.css'
+import * as postService from '../../services/postServices'
+import CategoryCarousel from './CategoryCarousel'
 
 export default function Forum () {
+  const [categories, setCategories] = useState();
+  const [category, setCategory] = useState();
+
+  useEffect(() => {
+    postService.getAllCategories()
+    .then(categories => setCategories(categories));
+    console.log(categories);
+  }, []);
 
   return (
     <main className={styles.container}>
       <h1>This is where the forum will be</h1>
+      <CategoryCarousel categories={categories}/>
     </main>
   )
 }
