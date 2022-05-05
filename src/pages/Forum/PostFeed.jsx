0d36e17d53b729563d778a6384fd2e2e8cfaf99c
jmolasky <jeromemolasky@gmail.com>
2022-05-05 03:11:55 -0500

    adds posts to forum

--- src/pages/Forum/PostFeed.jsx
+++ src/pages/Forum/PostFeed.jsx
@@ -1,17 +1,26 @@
 export default function PostFeed(props) {
-   
+
     return(
         <>
             <h1>{props.category.category}</h1>
             {props.category.posts.length > 0 ?
             <>
                 {props.category.posts.map((post) => (
-                    <div key={post._id}>{post.title}</div>
+                    <div 
+                        key={post._id}
+                        id={post._id}
+                        onClick={props.selectPost}
+                        >
+                            {post.title}
+                    </div>
                 ))}
             </>
             :
-            <p>No Posts in this Category</p>
+            <>
+                <p>No Posts in this Category</p>
+            </>
             }
         </>
+               
     )
 }
