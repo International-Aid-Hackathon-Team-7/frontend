4c087d7109e68c53bd37ed3e3444516dff32590d
Jan Horak <janny_b_goode@hotmail.com>
2022-05-07 23:40:56 -0400

    style create a post page

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 
 import { uploadFile } from "react-s3";
 
+import styles from "./CreatePost.module.css";
+
 window.Buffer = window.Buffer || require("buffer").Buffer;
 
 const S3_BUCKET = "bridge-app-bucket";
@@ -30,6 +32,7 @@ export default function CreatePost(props) {
   });
   const [categories, setCategories] = useState([]);
   const navigate = useNavigate();
+  const [fileName, setFileName] = useState("No File Chosen");
 
   useEffect(() => {
     getAllCategories().then((categories) => {
@@ -68,6 +71,7 @@ export default function CreatePost(props) {
 
   const handleChangePhoto = (evt) => {
     console.log("in the upload photo function: ", evt.target.files[0]);
+    setFileName(evt.target.files[0].name);
     uploadFile(evt.target.files[0], config)
       .then((data) => {
         console.log(data);
@@ -84,75 +88,89 @@ export default function CreatePost(props) {
 
   return (
     <>
-      <h1 className="post-title">Create a Post</h1>
-      <form
-        autoComplete="off"
-        ref={formElement}
-        onSubmit={handleSubmit}
-        className="create-post-form"
-      >
-        <div className="category-choice">
-          <select
-            onChange={(event) => handleChange(event)}
-            name="category"
-            id="selectCategory"
-          >
-            {categoryOptions}
-          </select>
-        </div>
-        <div className="form-group mb-3">
-          <label htmlFor="title-input" className="form-label">
-            Title<span>* </span>
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="title-input"
-            name="title"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-        </div>
+      <div className={styles.formContainer}>
+        <h1 className={styles.pageTitle}>Create Post</h1>
+        <form autoComplete="off" ref={formElement} onSubmit={handleSubmit}>
+          <div className={styles.titleInputContainer}>
+            <label htmlFor="title-input" className={styles.createPostTitle}>
+              <span>Title:</span>
+            </label>
+            <input
+              type="text"
+              className={styles.textInput}
+              id="title-input"
+              name="title"
+              value={formData.name}
+              onChange={handleChange}
+              placeholder="Enter title"
+              required
+            />
+          </div>
 
-        <div className="form-group mb-4">
-          <label htmlFor="content-input" className="form-label">
-            Post Content<span>* </span>
-          </label>
-          <textarea
-            type="text"
-            className="form-control"
-            id="content-input"
-            name="content"
-            value={formData.content}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group mb-4">
-          <label htmlFor="photo-upload" className="form-label">
-            Upload Photo
-          </label>
-          <input
-            type="file"
-            className="form-control"
-            id="media-upload"
-            name="media"
-            onChange={handleChangePhoto}
-          />
-        </div>
-        <div className="d-grid">
-          <button
-            type="submit"
-            className="btn btn-primary btn-fluid"
-            disabled={!validForm}
-          >
-            Add Post
-          </button>
-          <br />
-          <span>* </span> indicates a requried field
-        </div>
-      </form>
+          <div className={styles.contentInputContainer}>
+            <label htmlFor="content-input">
+              <span>Content:</span>
+            </label>
+            <textarea
+              type="text"
+              className={styles.contentInput}
+              id="content-input"
+              name="content"
+              value={formData.content}
+              onChange={handleChange}
+              placeholder="Enter content"
+              required
+            />
+            {/* <div classname={styles.wordsRemaining}>300 words remaining</div> */}
+          </div>
+
+          <div className={styles.mediaUploadContainer}>
+            Media:
+            <div className={styles.uploadLabel}>
+              <input
+                type="file"
+                className={styles.uploadButton}
+                id="media-upload"
+                name="media"
+                accept="image/*"
+                onChange={handleChangePhoto}
+              />
+              <label for="media-upload" className={styles.fileUploadButton}>
+                Choose File
+              </label>
+              <div className={styles.fileName}> {fileName}</div>
+            </div>
+          </div>
+
+          <div className={styles.categorySelectionContainer}>
+            <span>Category:</span>
+            <select
+              onChange={(event) => handleChange(event)}
+              name="category"
+              id="selectCategory"
+              className={styles.categorySelection}
+            >
+              {categoryOptions}
+            </select>
+          </div>
+
+          <div className={styles.postAnonymously}>
+            <label for="anonymous" className={styles.switch}>
+              <input type="checkbox" id="anonymous"/>
+              <span className={styles.slider}></span>
+            </label>
+              Post Anonymously
+          </div>
+
+            <button
+              type="submit"
+              className={styles.submitBtn}
+              disabled={!validForm}
+            >
+              Create Post
+            </button>
+        </form>
+      </div>
     </>
   );
 }
