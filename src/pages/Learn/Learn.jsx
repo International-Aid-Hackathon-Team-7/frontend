19f762b81251ed35d25588732d7186d67565a44c
lukden <dean.l@northeastern.edu>
2022-05-17 20:11:07 -0400

    delete responsive section and change logo

--- src/pages/Learn/Learn.jsx
+++ src/pages/Learn/Learn.jsx
@@ -7,23 +7,6 @@ import styles from './Learn.module.css';
 
 const Climate = props => {
 
-    const responsive = {
-      desktop: {
-        breakpoint: { max: 3000, min: 1024 },
-        items: 3,
-        partialVisibilityGutter: 40 // this is needed to tell the amount of px that should be visible.
-      },
-      tablet: {
-        breakpoint: { max: 1024, min: 464 },
-        items: 2,
-        partialVisibilityGutter: 30 // this is needed to tell the amount of px that should be visible.
-      },
-      mobile: {
-        breakpoint: { max: 464, min: 0 },
-        items: 1,
-        partialVisibilityGutter: 30 // this is needed to tell the amount of px that should be visible.
-      }
-    }
   
     return (
       <>
